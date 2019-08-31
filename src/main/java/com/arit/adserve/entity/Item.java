8d00d3ceada8f0c3736a59415ece3d5aebfd4e4c
Alex <ar@alryj.com>
2019-08-31 19:24:45 +0800

    intial rest api setup

---
+++ src/main/java/com/arit/adserve/entity/Item.java
@@ -0,0 +1,21 @@
+package com.arit.adserve.entity;
+
+public class Item {
+
+	private String name;
+	private int price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
---
+++ src/test/java/com/arit/adserve/controller/ItemControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.arit.adserve.controller;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ItemControllerIntegrationTest {
+	
+	@LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(),
+                String.class);
+        assertTrue(response.getBody().equals("Greetings from Spring Boot!"));
+    }
+}
