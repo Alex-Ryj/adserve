ac57941793ae99391e9aa5e690b09d7c02c64ee6
nico <mymail@gmail.com>
2020-02-23 19:35:51 +0800

    some fop image tests

--- src/main/java/com/arit/adserve/image/Utils.java
+++ src/main/java/com/arit/adserve/image/Utils.java
@@ -1,12 +1,19 @@
 package com.arit.adserve.image;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOInvalidTreeException;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.imageio.stream.ImageOutputStream;
 
 import org.springframework.data.util.Pair;
 
@@ -70,6 +77,37 @@ public class Utils {
 		gr2.drawImage(img2, null, img1.getWidth() + offset, 0);
 		gr2.dispose();
 		return newImage;
+	}
+	
+	public byte[] pngWriteCustomData(BufferedImage buffImg, String key, String value) throws IOException {
+	    ImageWriter writer = ImageIO.getImageWritersByFormatName("png").next();
+
+	    ImageWriteParam writeParam = writer.getDefaultWriteParam();
+	    ImageTypeSpecifier typeSpecifier = ImageTypeSpecifier.createFromBufferedImageType(BufferedImage.TYPE_INT_RGB);
+
+	    //adding metadata
+	    IIOMetadata metadata = writer.getDefaultImageMetadata(typeSpecifier, writeParam);
+
+	    IIOMetadataNode textEntry = new IIOMetadataNode("tEXtEntry");
+	    textEntry.setAttribute("keyword", key);
+	    textEntry.setAttribute("value", value);
+
+	    IIOMetadataNode text = new IIOMetadataNode("tEXt");
+	    text.appendChild(textEntry);
+
+	    IIOMetadataNode root = new IIOMetadataNode("javax_imageio_png_1.0");
+	    root.appendChild(text);
+
+	    metadata.mergeTree("javax_imageio_png_1.0", root);
+
+	    //writing the data
+	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    ImageOutputStream stream = ImageIO.createImageOutputStream(baos);
+	    writer.setOutput(stream);
+	    writer.write(metadata, new IIOImage(buffImg, null, metadata), writeParam);
+	    stream.close();
+
+	    return baos.toByteArray();
 	}
 
 }
