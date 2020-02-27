922d2544a67f6decb906a96f9538b8187d70ce43
RYJOUKHINE Alexandre <aryjoukhine@one.steria.dom>
2020-02-27 20:45:17 +0800

    fop tests corrections
    Conflicts:
    	src/test/java/com/arit/adserve/fop/FopTest.java
    	src/test/resources/fop/emp.png

--- src/main/java/com/arit/adserve/providers/ebay/EBayFindRequest.java
+++ src/main/java/com/arit/adserve/providers/ebay/EBayFindRequest.java
@@ -4,45 +4,34 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EBayFindRequest implements Serializable {
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-    private static Map<String, String> endpoints = new HashMap<>();
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@ToString
+public class EBayFindRequest implements Serializable {
+  
+	private static final long serialVersionUID = 1L;
+	
+	private static Map<String, String> endpoints = new HashMap<>();
     private int pageNumber;
     private int totalPages;
-    private String serachWords;
-    private int totalUpdatedItems;
-
-
-    public enum RequestState {
-        RETRIEVE_ITEMS,
-        UPDATE_ITEMS
-    }
-
-    public enum Operations {
-
-    }
-
-
+    private int itemsPerPage;
+    private int itemsTotalInRequest;
+    private String searchWords;    
+    private int maxRequiredItems;
+    private int itemsUpdatedToday;
+    private int itemsTotal;
+    private RequestState state;
 
     static {
         endpoints.put("Finding", "https4://svcs.ebay.com/services/search/FindingService/v1?");
         endpoints.put("Shopping", "http4://open.api.ebay.com/shopping?");
         endpoints.put("SOAP", "https4://api.ebay.com/wsapi");
-    }
+    }  
 
-    public int getPageNumber() {
-        return pageNumber;
-    }
-
-    public void setPageNumber(int pageNumber) {
-        this.pageNumber = pageNumber;
-    }
-
-    public int getTotalPages() {
-        return totalPages;
-    }
-
-    public void setTotalPages(int totalPages) {
-        this.totalPages = totalPages;
-    }
 }
--- src/test/java/com/arit/adserve/rules/DroolsTest.java
+++ src/test/java/com/arit/adserve/rules/DroolsTest.java
@@ -1,6 +1,6 @@
 package com.arit.adserve.rules;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 
@@ -9,6 +9,8 @@ import org.junit.Test;
 import org.kie.api.runtime.KieSession;
 
 import com.arit.adserve.entity.Item;
+import com.arit.adserve.providers.ebay.EBayFindRequest;
+import com.arit.adserve.providers.ebay.RequestState;
 
 public class DroolsTest {
 
@@ -25,8 +27,21 @@ public class DroolsTest {
         item.setPrice(7000);
         item.setCondition("Used");
         kSession.insert(item); 
+        
+        EBayFindRequest fr = new EBayFindRequest();
+        fr.setState(RequestState.INITIAL);
+        kSession.insert(fr);
         kSession.fireAllRules(); 
         assertTrue(item.isProcess());
     }
+    
+    @Test
+	public void testEBayREquest() throws Exception {
+    	  EBayFindRequest fr = new EBayFindRequest();
+          fr.setState(RequestState.INITIAL);
+          kSession.insert(fr);
+          kSession.fireAllRules(); 
+          assertEquals(RequestState.INITIAL, fr.getState());
+	}
 
 }
